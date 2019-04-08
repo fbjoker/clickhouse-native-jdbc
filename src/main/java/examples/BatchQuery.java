72946a62b18d9309723275102b257fb97861204d
fbjoker <39256912@qq.com>
2019-04-08 11:41:41 +0800

    测试TCP的方式插入数据

--- src/main/java/examples/BatchQuery.java
+++ src/main/java/examples/BatchQuery.java
@@ -1,10 +1,15 @@
 package examples;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.awt.*;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static examples.utils.DataUtils.getRandomIp;
+import static examples.utils.DataUtils.nextTime;
 
 /**
  */
@@ -12,6 +17,7 @@ public class BatchQuery {
 
     public static void main(String[] args) throws Exception {
 
+//        int fieldNumber = 420;
         int fieldNumber = 420;
 
         String fieldsType = "String, String, String, String, String, String, String, String, String, " +
@@ -45,32 +51,72 @@ public class BatchQuery {
                 "Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, " +
                 "Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Long, Long, Long, Long, " +
                 "Long, Double, Double";
-        String[] partner= {"YNYDZX","YNYDHW","JS_CMCC_CP","JS_CMCC_CP_ZX","HNYD","SD_CMCC_JN","LNYD","SAXYD"};
+        String[] partners = {"YNYDZX", "YNYDHW", "JS_CMCC_CP", "JS_CMCC_CP_ZX", "HNYD", "SD_CMCC_JN", "LNYD", "SAXYD"};
         // Initialize client (endpoint, username, password)
 
-        String table=args[0];
-        Long iterNum=Long.parseLong(args[1]);
+        String table = args[0];
+        Long iterTime = Long.parseLong(args[1]);
+       String partner=partners[new Random().nextInt(8)];
+        Long iterNum=1000000L;
+        for (int i = 0; i <iterTime ; i++) {
+
+        producer2InsertClickHouse(fieldNumber, fieldsType,partner , table, iterNum);
+        }
+    }
+
+    private static void producer2InsertClickHouse(int fieldNumber, String fieldsType, String partner, String table, Long iterNum) throws ClassNotFoundException, SQLException {
         Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
         Connection connection = DriverManager.getConnection("jdbc:clickhouse://10.10.121.213:9000");
 
-        StringBuilder params= new StringBuilder();
-        for (int i = 0; i <fieldNumber ; i++) {
+        StringBuilder params = new StringBuilder();
+        for (int i = 0; i < fieldNumber - 1; i++) {
             params.append("?,");
         }
+        String param = params.append("?").toString();
 
         Statement stmt = connection.createStatement();
-        stmt.executeQuery("drop table if exists test_jdbc_example");
-        stmt.executeQuery("create table test_jdbc_example(day Date, name String, age UInt8) Engine=Log");
+//        stmt.executeQuery("drop table if exists test_jdbc_example");
+//        stmt.executeQuery("create table test_jdbc_example(day Date, name String, age UInt8) Engine=Log");
 
-        PreparedStatement pstmt = connection.prepareStatement("INSERT INTO test_jdbc_example VALUES(?, ?, ?)");
+        PreparedStatement pstmt = connection.prepareStatement("INSERT INTO " + table + " VALUES(" + param + ")");
+        System.out.println("INSERT INTO " + table + " VALUES(" + param + ")");
+        long startCreatData=System.currentTimeMillis();
 
-        for (int i = 0; i < 20000; i++) {
-            pstmt.setDate(1, new Date(System.currentTimeMillis()));
-            pstmt.setString(2, "Zhang San" + i);
-            pstmt.setByte(3, (byte)i);
-            pstmt.addBatch();
+        for (int iter = 0; iter < iterNum; iter++) {
+            pstmt.setInt(1, ThreadLocalRandom.current().nextInt(100000));
+            pstmt.setString(2, partner);
+            pstmt.setString(3, new SimpleDateFormat("yyyy-MM-dd ").format(nextTime()));
+            pstmt.setString(4, getRandomIp());
+
+            String[] fieldsTypeList = fieldsType.split(",");
+            for (int i = 4; i < fieldNumber; i++) {
+                switch (fieldsTypeList[i -4].trim()) {
+//                case "String":data[i]="abcdefg";
+                    case "String":
+                        pstmt.setString(i+1, RandomStringUtils.randomAlphanumeric(new Random().nextInt(8) + 1));
+                        break;
+                    case "Int":
+                        pstmt.setInt(i+1, ThreadLocalRandom.current().nextInt(10000));
+                        break;
+                    case "Long":
+                        pstmt.setLong(i+1, ThreadLocalRandom.current().nextLong(1000000));
+                        break;
+                    case "Double":
+                        pstmt.setDouble(i+1, ThreadLocalRandom.current().nextDouble(500));
+                        break;
+                    default:
+                        pstmt.setByte(i+1, Byte.parseByte("1"));
+                        break;
+                }
+            }
+        pstmt.addBatch();
         }
+        long endCreatData = System.currentTimeMillis();
+        System.out.println("本批次生成数据时间："+(endCreatData-startCreatData));
+        long startInsert = System.currentTimeMillis();
         pstmt.executeBatch();
+        long endInsert = System.currentTimeMillis();
+        System.out.println("本批次插入时间："+(endInsert-startInsert));
         //stmt.executeQuery("drop table test_jdbc_example");
     }
 }
